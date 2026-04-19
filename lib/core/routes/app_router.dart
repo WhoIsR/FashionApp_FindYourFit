c367c39ffe9966ca5f9684b3be997e6f3a4d6cbf
radja <radjasatrio70@gmail.com>
2026-04-19 20:08:45 +0700

    feat: mengatur navigasi antar halaman
    Mendaftarkan jalur rute untuk halaman splash, login, pendaftaran, dan verifikasi email. Tujuannya agar perpindahan layar dalam aplikasi menjadi lebih rapi dan terpusat.

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../../features/auth/presentation/pages/splash_page.dart';
+import '../../features/auth/presentation/pages/login_page.dart';
+import '../../features/auth/presentation/pages/register_page.dart';
+import '../../features/auth/presentation/pages/verify_email_page.dart';
+
+class AppRouter {
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String verifyEmail = '/verify-email';
+
+  static Map<String, WidgetBuilder> get routes => {
+    splash: (_) => const SplashPage(),
+    login: (_) => const LoginPage(),
+    register: (_) => const RegisterPage(),
+    verifyEmail: (_) => const VerifyEmailPage(),
+  };
+}
