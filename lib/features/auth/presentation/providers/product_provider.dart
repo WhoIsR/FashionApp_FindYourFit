66a4ef854a2f245478f89ee91e7159bacc5785fa
radja <radjasatrio70@gmail.com>
2026-04-20 14:31:43 +0700

    feat: menambahkan pengatur state untuk katalog
    Membuat ProductProvider untuk mengelola status antarmuka. Provider ini mengatur kapan layar harus menampilkan animasi memuat data, memunculkan daftar pakaian, atau menampilkan pesan error.

---
+++ lib/features/auth/presentation/providers/product_provider.dart
@@ -0,0 +1,33 @@
+import 'package:fashion_app/features/auth/data/repositories/prodcut_repository_impl.dart';
+import 'package:flutter/material.dart';
+import '../../domain/repositories/product_repository.dart';
+import '../../data/models/product_model.dart';
+
+enum ProductStatus { initial, loading, loaded, error }
+
+class ProductProvider extends ChangeNotifier {
+  final ProductRepository _repository = ProductRepositoryImpl();
+
+  ProductStatus _status = ProductStatus.initial;
+  List<ProductModel> _products = [];
+  String? _error;
+
+  ProductStatus get status => _status;
+  List<ProductModel> get products => _products;
+  String? get error => _error;
+
+  Future<void> fetchProducts() async {
+    _status = ProductStatus.loading;
+    notifyListeners();
+
+    try {
+      _products = await _repository.getProducts();
+      _status = ProductStatus.loaded;
+    } catch (e) {
+      _error = e.toString();
+      _status = ProductStatus.error;
+    }
+
+    notifyListeners();
+  }
+}
