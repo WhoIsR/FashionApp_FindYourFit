3d81f209593cf679f78decce321f14d7cbc250d3
radja <radjasatrio70@gmail.com>
2026-04-21 23:48:46 +0700

    style: menambahkan dependesni  Google Fonts dan pembaruan AppColors
    Menambahkan font kustom melalui Google Fonts pada konfigurasi pubspec.yaml serta mengubah skema warna utama di dalam file app_colors.dart agar visual aplikasi selaras dengan tema desain UI/UX yang baru.

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color surface = Color(0xFFFAF9F6); // Background utama krem
-  static const Color onSurface = Color(0xFF2F3430); // Teks hitam/charcoal
-  static const Color onSurfaceVariant = Color(0xFF5C605C); // Teks abu-abu
-  static const Color secondary = Color(0xFF7C5900); // Aksen emas
-  static const Color secondaryDim = Color(
-    0xFF6D4E00,
-  ); // Emas gelap buat gradient
-  static const Color surfaceContainerLowest = Color(0xFFFFFFFF); // Putih bersih
-  static const Color surfaceContainer = Color(
-    0xFFEDEEEA,
-  ); // Abu-abu muda buat blok
-  static const Color outlineVariant = Color(0xFFAFB3AE); // Ghost border
-  static const Color inverseSurface = Color(
-    0xFF0D0F0D,
-  ); // Background gelap buat splash dan gradasi
+  static const Color primary = Color(0xFF5F5E5E);
+  static const Color secondary = Color(0xFF7C5900);
+  static const Color secondaryDim = Color(0xFF6D4E00); 
+
+  static const Color background = Color(0xFFFAF9F6);
+  static const Color surface = Color(0xFFFAF9F6);
+  static const Color surfaceDim = Color(0xFFD6DBD5);
+  
+  static const Color surfaceContainerLowest = Color(0xFFFFFFFF);
+  static const Color surfaceContainerLow = Color(0xFFF4F4F0);
+  static const Color surfaceContainer = Color(0xFFEDEEEA);
+  static const Color surfaceContainerHigh = Color(0xFFE6E9E4);
+  static const Color surfaceContainerHighest = Color(0xFFE0E4DE);
+
+  static const Color onSurface = Color(0xFF2F3430);
+  static const Color onSurfaceVariant = Color(0xFF5C605C);
+
+  static const Color outline = Color(0xFF777C77);
+  static const Color outlineVariant = Color(0xFFAFB3AE);
+  
+  static const Color inverseSurface = Color(0xFF0D0F0D);
+  static const Color inverseOnSurface = Color(0xFF9D9D9A);
 }
